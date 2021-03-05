2f3ff580590600fe81a84e7c1103bda77d9fc5a3
Leandro <leandro.klingeroliveira@gmail.com>
2021-03-05 10:43:09 -0300

    Project Created

---
+++ src/main/java/com/klinger/JavaProjectOne/JavaProjectOneApplication.java
@@ -0,0 +1,13 @@
+package com.klinger.JavaProjectOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klinger/JavaProjectOne/JavaProjectOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klinger.JavaProjectOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
