5e12fa68c20863cfc6209f464d9facca8d3c1cf4
Leandro <leandro.klingeroliveira@gmail.com>
2021-03-10 09:23:01 -0300

    Ajustes no endpoint / categorias / {id}

---
+++ src/main/java/com/klinger/JavaProjectOne/resources/Exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.klinger.JavaProjectOne.resources.Exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.klinger.JavaProjectOne.service.exceptions.ObjectNotFoundException;
+
+import jdk.jfr.Timespan;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(),e.getMessage(),System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+}
