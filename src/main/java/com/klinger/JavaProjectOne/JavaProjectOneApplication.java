66edf3e88530e7417e4ea37f634b918aa7f382cf
Leandro <leandro.klingeroliveira@gmail.com>
2021-03-10 08:37:56 -0300

    Criando operacao de instanciacao

--- src/main/java/com/klinger/JavaProjectOne/JavaProjectOneApplication.java
+++ src/main/java/com/klinger/JavaProjectOne/JavaProjectOneApplication.java
@@ -1,13 +1,32 @@
 package com.klinger.JavaProjectOne;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class JavaProjectOneApplication {
+import com.klinger.JavaProjectOne.domain.Categoria;
+import com.klinger.JavaProjectOne.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class JavaProjectOneApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository; 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JavaProjectOneApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null,"informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
